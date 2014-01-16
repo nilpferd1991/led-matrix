5c246b38005130b9f5b52110f694d19a46432604
Markus Prim <prime@leech.it>
2014-01-16 23:40:57 +0100

    Slowly building up the main routine.

--- Code/main.cpp
+++ Code/main.cpp
@@ -6,8 +6,16 @@
 // Description :
 //============================================================================
 
+#include <Field.h>
 
 int main() {
+
+	// this is hardware restricted
+	const unsigned int nXLEDs(8);
+	const unsigned int nYLEDs(8);
+	// setup conways game of life
+	Field field(nXLEDs, nYLEDs);
+
 
 	while(true) {
 
