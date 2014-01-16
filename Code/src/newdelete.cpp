21d595c77cc6159ed2306d9342e61fc1dfb1f25b
Nils <area51.nils@googlemail.com>
2014-01-16 23:57:16 +0100

    Added hardware interface

---
+++ Code/src/newdelete.cpp
@@ -0,0 +1,24 @@
+extern "C"
+{
+  #include <stdlib.h>
+}
+
+void *operator new(size_t size)
+{
+  return malloc(size);
+}
+
+void *operator new[](size_t size)
+{
+  return malloc(size);
+}
+
+void operator delete(void* ptr)
+{
+  free(ptr);
+}
+
+void operator delete[](void* ptr)
+{
+  free(ptr);
+}
