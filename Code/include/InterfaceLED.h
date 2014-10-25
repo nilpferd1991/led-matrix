b8f44db90352dd9bc6772eb9aeaa8537c2edc0e1
Nils <area51.nils@googlemail.com>
2014-10-25 14:19:27 +0200

    Added PORT implemention

--- Code/include/InterfaceLED.h
+++ Code/include/InterfaceLED.h
@@ -13,6 +13,9 @@
 #ifndef INTERFACELED_H_
 #define INTERFACELED_H_
 
+#define OUTPUT_PORT PORTB
+#define OUTPUT_PORT_DDR DDRB
+
 /**
  * Class to interface the programm with the LED matrix via the shift registers
  * Sends signals to the shift registers to turn the LEDs on and off. Should be uses in the mainloop to
@@ -26,10 +29,8 @@ public:
 	 * and sets the output to Off before any actions.
 	 */
 	InterfaceLED() {
-		DDRB = 0xFF;
-		PORTB = 0;
-		DDRC = 0xFF;
-		PORTC = 0;
+		OUTPUT_PORT_DDR = 0xFF;
+		OUTPUT_PORT = 0;
 	}
 
 	void showCycles(const Field * const field, const uint16_t cycles) const;
@@ -39,53 +40,53 @@ private:
 	void showField(const Field * const field, const uint16_t waitingTime = 1000) const;
 
 	void enableOutput() const {
-		PORTB |= (1 << InterfaceLED::m_outputEnablePin);
+		OUTPUT_PORT |= (1 << InterfaceLED::m_outputEnablePin);
 	}
 	void disableOutput() const {
-		PORTB &= ~(1 << InterfaceLED::m_outputEnablePin);
+		OUTPUT_PORT &= ~(1 << InterfaceLED::m_outputEnablePin);
 	}
 
 	void enableWritingColumns() const {
-		PORTB &= ~(1 << InterfaceLED::m_strobePin);
+		OUTPUT_PORT &= ~(1 << InterfaceLED::m_strobePin_1);
 	}
 	void disableWritingColumns() const {
-		PORTB |= (1 << InterfaceLED::m_strobePin);
+		OUTPUT_PORT |= (1 << InterfaceLED::m_strobePin_1);
 	}
 
 	void enableWritingRows() const {
-		PORTC &= ~(1 << InterfaceLED::m_strobePin);
+		OUTPUT_PORT &= ~(1 << InterfaceLED::m_strobePin_2);
 	}
 	void disableWritingRows() const {
-		PORTC |= (1 << InterfaceLED::m_strobePin);
+		OUTPUT_PORT |= (1 << InterfaceLED::m_strobePin_2);
 	}
 
 	void tickColumns() const {
-		PORTB |= (1 << InterfaceLED::m_clockPin);
-		PORTB &= ~(1 << InterfaceLED::m_clockPin);
+		OUTPUT_PORT |= (1 << InterfaceLED::m_clockPin_1);
+		OUTPUT_PORT &= ~(1 << InterfaceLED::m_clockPin_1);
 	}
 
 	void tickRows() const {
-		PORTC |= (1 << InterfaceLED::m_clockPin);
-		PORTC &= ~(1 << InterfaceLED::m_clockPin);
+		OUTPUT_PORT |= (1 << InterfaceLED::m_clockPin_2);
+		OUTPUT_PORT &= ~(1 << InterfaceLED::m_clockPin_2);
 	}
 
 	void setColumn(const bool status) const {
 		if (status) {
-			PORTB |= (1 << InterfaceLED::m_dataPin);
+			OUTPUT_PORT |= (1 << InterfaceLED::m_dataPin_1);
 		}
 		else
 		{
-			PORTB &= ~(1 << InterfaceLED::m_dataPin);
+			OUTPUT_PORT &= ~(1 << InterfaceLED::m_dataPin_1);
 		}
 	}
 
 	void setRow(const bool status) const {
 		if (status) {
-			PORTC |= (1 << InterfaceLED::m_dataPin);
+			OUTPUT_PORT |= (1 << InterfaceLED::m_dataPin_2);
 		}
 		else
 		{
-			PORTC &= ~(1 << InterfaceLED::m_dataPin);
+			OUTPUT_PORT &= ~(1 << InterfaceLED::m_dataPin_2);
 		}
 	}
 
@@ -95,17 +96,20 @@ private:
 	/**
 	 * m_strobePin is the pin of the STROBE on Port B.
 	 */
-	static const uint8_t m_strobePin = 2;
+	static const uint8_t m_strobePin_1 = 2;
+	static const uint8_t m_strobePin_2 = 6;
 
 	/**
 	 * m_strobePin is the pin of the DATA on Port B.
 	 */
-	static const uint8_t m_dataPin = 1;
+	static const uint8_t m_dataPin_1 = 1;
+	static const uint8_t m_dataPin_2 = 5;
 
 	/**
 	 * m_strobePin is the pin of the CLOCK on Port B.
 	 */
-	static const uint8_t m_clockPin = 0;
+	static const uint8_t m_clockPin_1 = 0;
+	static const uint8_t m_clockPin_2 = 4;
 
 	/**
 	 * m_enableOutputPin is the pin of the ENABLEOUTPUT on Port B.
